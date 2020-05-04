24688e93567851a9250b45784ec9da22e362eb36
Priyanka <fulmogarepriya@gmail.com>
2020-05-05 00:49:07 +0530

    Updated the Store result

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -8,4 +8,8 @@ OPeration1=$(($Number1+$Number2*$Number3));
 OPeration2=$(($Number1%$Number2+$Number3));
 OPeration3=$(($Number1+$Number2/$Number3));
 OPeration4=$(($Number1*$Number2+$Number3));
+Result[1]="$Operation1"
+Result[2]="$Operation2"
+Result[3]="$Operation3"
+Result[4]="$Operation4"
 
