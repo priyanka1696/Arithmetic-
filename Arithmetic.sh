7d91b876f829371b2a4c63c264871931f94d3414
Priyanka <fulmogarepriya@gmail.com>
2020-05-04 13:23:40 +0530

    Updated The Values Into Array

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -12,4 +12,18 @@ Result[1]="$Operation1"
 Result[2]="$Operation2"
 Result[3]="$Operation3"
 Result[4]="$Operation4"
+echo ${result[@]}
+arrayLength=${#result[@]}
+for ((i=0;i<$arrayLength;i++))
+do
+  for ((j=i+1;j<$arrayLength;j++))
+  do
+    if [ ${result[i]} -gt ${result[j]} ];
+    then
+        temp=${result[i]};
+        result[$i]=${result[j]};
+        result[$j]=$temp;
+    fi
+   done
+  done
 
