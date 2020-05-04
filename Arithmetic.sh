fb199894fb11ebeab0d8bc617ef7939d0caef1aa
Priyanka <fulmogarepriya@gmail.com>
2020-05-05 00:32:12 +0530

    Updated the fourth Operation

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -7,4 +7,5 @@ read number3
 OPeration1=$(($Number1+$Number2*$Number3));
 OPeration2=$(($Number1%$Number2+$Number3));
 OPeration3=$(($Number1+$Number2/$Number3));
+OPeration4=$(($Number1*$Number2+$Number3));
 
