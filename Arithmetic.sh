417114b4676634c83f56e83a8843c19a98686320
Priyanka <fulmogarepriya@gmail.com>
2020-05-04 13:06:52 +0530

    Updated The ArithmeticOperation

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -4,4 +4,8 @@ echo "Enter Three Number"
 read number1
 read number2
 read number3
+OPeration1=$(($Number1+$Number2*$Number3));
+OPeration2=$(($Number1%$Number2+$Number3));
+OPeration3=$(($Number1+$Number2/$Number3));
+OPeration4=$(($Number1*$Number2+$Number3));
 
