37d16eeec94578818a7aa5c392c1a8cfd9bc0d1d
Priyanka <fulmogarepriya@gmail.com>
2020-05-07 13:20:45 +0530

    Sort the result in ascending order

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -x
 echo "Welcome to Sorting Arithmetic Computation Problem"
-echo "Enter Three Number"
-read number1
-read number2
-read number3
-OPeration1=$(($Number1+$Number2*$Number3));
-OPeration2=$(($Number1%$Number2+$Number3));
-OPeration3=$(($Number1+$Number2/$Number3));
-OPeration4=$(($Number1*$Number2+$Number3));
-Result[1]="$Operation1"
-Result[2]="$Operation2"
-Result[3]="$Operation3"
-Result[4]="$Operation4"
+echo "Enter Three Number";
+read Number1;
+read Number2;
+read Number3;
+operation1=$(($Number1+$Number2*$Number3));
+operation2=$(($Number1%$Number2+$Number3));
+operation3=$(($Number1+$Number2/$Number3));
+operation4=$(($Number1*$Number2+$Number3));
+
+result[0]="$operation1"
+result[1]="$operation2"
+result[2]="$operation3"
+result[3]="$operation4"
 echo ${result[@]}
 arrayLength=${#result[@]}
 for ((i=0;i<$arrayLength;i++))
@@ -20,10 +21,11 @@ do
   do
     if [ ${result[i]} -gt ${result[j]} ];
     then
-        temp=${result[i]};
-        result[$i]=${result[j]};
-        result[$j]=$temp;
+                temp=${result[i]};
+                result[$i]=${result[j]};
+                result[$j]=$temp;
     fi
-   done
-  done
-
+        done
+done
+echo ${result[@]};
+echo "Ascending Order= "${result[@]}
